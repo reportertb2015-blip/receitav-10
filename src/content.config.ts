f1c08fcd5c4055a3fbb6b9a3d42d1bcd2cb2c95f
reportertb2015-blip <reportertb2015@gmail.com>
2026-05-14 11:52:23 -0300

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,19 @@
-import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    category: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-  }),
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
 
-export default {
-  blog,
-};
+export const collections = { blog };
