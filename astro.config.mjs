3354c785a96a6ad4535d4ae954ffa033403238a6
reportertb2015-blip <reportertb2015@gmail.com>
2026-05-13 23:49:24 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://receitagratis.com", // Atualizado para o seu domínio
+	output: "server", // ADICIONE ISSO: Ativa o SSR para que o Cloudflare Workers funcione corretamente
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
+		mode: "directory", // Recomendado para Cloudflare Pages
 		platformProxy: {
 			enabled: true,
 		},
