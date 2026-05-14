8fa7fcc1738613ce6656afb944ac7b3e82bfe157
reportertb2015-blip <reportertb2015@gmail.com>
2026-05-13 21:50:31 -0300

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,6 +13,9 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		// ADICIONE ESTAS DUAS LINHAS ABAIXO:
+		category: z.string().optional(),
+		tags: z.array(z.string()).optional(),
 	}),
 });
 
